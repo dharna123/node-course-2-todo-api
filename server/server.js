cf612f990817200c90fa20e57e2b7dde0d454c0d
Dharna <dharnaahuja123@gmail.com>
2018-12-18 11:49:39 +0530

    Add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,120 +1,51 @@
-var express=require('express');
-var bodyParser=require('body-parser');
-var {ObjectID}=require('mongodb');
+var express = require('express');
+var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
-var {mongoose}=require('./db/mongoose');
-var {Todo}=require('./models/todo');
-var {User}=require('./models/user');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var app=express();
-const port=process.env.PORT || 3000;
+var app = express();
 
 app.use(bodyParser.json());
-app.post('/todos',(req,res) => {
-    var todo=new Todo({
-        text: req.body.text
-    })
 
-    todo.save().then((doc)=>{
-        res.send(doc);
-    },(e) => {
-        res.status(400).send(e);
-    });
-    });
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
 
-    //GET /todos/1234324
-    app.get('/todos/:id', (req,res) => {
-        var id=req.paramms.id;
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
 
-        if(!ObjectID.isValid(id)){
-            return res.status(404).send();
-        }
+app.get('/todos/:id', (req, res) => {
+    var id=req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
 Todo.findById(id).then((todo)=> {
-if(!todo){
-    return res.status(404).send();
-}
+    if(!todo){
+        return res.status(404).send();
+    }
 
-res.send({todo});
-}).catch((e)=> {
+    res.send({todo});
+}).catch((e) => {
     res.status(400).send();
 })
 
-       
-    });
-
-
-/*app.get('/todos',(req,res) => {
-    Todo.find().then((todos) => {
-res.send({todos});
-    },(e) => {
-res.status(400).send(e);
+  
     })
+  
+   
 
-});*/
 
-app.listen(port,() => {
-    console.log(`Started up at port ${port}`);
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
 
-
-module.exports={app};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*var newTodo=new Todo({
-    text: 'Cook dinner'
-});
-//save new something
-
-newTodo.save().then((doc)=>{
-    console.log('Saved todo',doc);
-}, (e)=> {
-    console.log('Unable to save todo');
-});
-*/
-
-/*var otherTodo=new Todo({
-    text: 'Something to do'
-});
-
-
-otherTodo.save().then((doc)=>{
-    console.log(JSON.stringify(doc, undefined, 2));
-}, (e) => {
-    console.log('Unable to save',e);
-});
-
-//User 
-//email- require it-trim it-set type-set minlength of 1
-
-
-var user=new User({
-email: 'andrew@example.com  '
-});
-
-user.save().then((doc)=> {
-    console.log('User saved',doc)
-},(e)=> {
-console.log('Unable to save user',e);
-}
-);*/
+module.exports = {app};
