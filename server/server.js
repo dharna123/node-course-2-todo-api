b7c109de6d4bbff4ea6fcbc148941386f824a529
Dharna <dharnaahuja123@gmail.com>
2018-12-18 16:10:20 +0530

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -39,18 +39,16 @@ app.get('/todos/:id', (req, res) => {
         return res.status(404).send();
     }
 
-Todo.findById(id).then((todo)=> {
-    if(!todo){
-        return res.status(404).send();
-    }
+    Todo.findById(id).then((todo)=> {
+        if(!todo){
+            return res.status(404).send();
+        }
 
-    res.send({todo});
-}).catch((e) => {
-    res.status(400).send();
-})
-
-  
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
     })
+})
   
    app.delete('/todos/:id',(req,res)=>{
        //get the id
@@ -60,12 +58,13 @@ Todo.findById(id).then((todo)=> {
             return res.status(404).send();
         }
        //remove todo by id
+       
        Todo.findByIdAndRemove(id).then((todo)=> {
            if(!todo){
                return res.status(404).send();
            }
 
-           res.send(todo);
+           res.send({todo});
 
        }).catch((e)=> {
            res.status(400).send();
