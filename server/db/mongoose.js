1a767a5180327b9858e5dab581c4dca746965824
Dharna <dharnaahuja123@gmail.com>
2018-12-18 12:53:09 +0530

    Add mongolab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI||'mongodb://dharna:Love2l@ugh@ds137634.mlab.com:37634/todoapp');
 
 module.exports={mongoose};
 
